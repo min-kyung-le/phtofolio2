92faf20fb5e9a80683ef0b4b966a523eb2999b28
mink <minkyung1435@gmail.com>
2023-03-28 15:57:01 +0900

    초기설정

---
+++ src/components/Main.vue
@@ -0,0 +1,23 @@
+<template>
+  <span>글자를 입력하시오.</span
+  ><input
+    :value="state.text"
+    @input="(event) => (state.text = event.target.value)"
+  />
+  <h2>{{ state.text }}</h2>
+</template>
+
+<script lang="ts">
+import { reactive } from "vue";
+
+export default {
+  setup() {
+    const state = reactive({ text: "텍스트 입력" });
+    return {
+      state,
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss"></style>
