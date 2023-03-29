a46fae955a4c68bb4a0335c3bf2b61c9b00d2d87
mink <minkyung1435@gmail.com>
2023-03-29 23:48:22 +0900

    컴포넌트이름변경 반영

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,18 @@
   <Main />
   <HomeLayout />
   <About />
-  <Skill />
+  <Skill1 />
   <Skill2 />
-  <Career />
+  <Career1 />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import Main from "./components/Main.vue";
 import About from "./components/About.vue";
-import Skill from "./components/Skill.vue";
+import Skill1 from "./components/Skill1.vue";
 import Skill2 from "./components/Skill2.vue";
-import Career from "./components/Career.vue";
+import Career from "./components/Career1.vue";
 import HomeLayout from "@/layout/HomeLayout.vue";
 
 export default defineComponent({
@@ -23,9 +23,9 @@ export default defineComponent({
     Main,
     HomeLayout,
     About,
-    Skill,
+    Skill1,
     Skill2,
-    Career,
+    Career1,
   },
 
   data() {
