bbfb8e7eaf2f60ce91082aae9b1169f59628f852
mink <minkyung1435@gmail.com>
2023-03-30 13:29:25 +0900

    메뉴 클릭효과 추가

--- src/components/Done.vue
+++ src/components/Done.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="done page">
+    <div class="menu-contents">
+      <div class="name-div">
+        <div
+          class="name"
+          v-for="(one, idx) in menuList"
+          @click="menuClick(idx)"
+        >
+          {{ one.name }}
+          <div
+            class="line"
+            :class="one.isClick === false ? 'opacity-0' : 'opacity-1'"
+          ></div>
+        </div>
+      </div>
+    </div>
     <div class="menu-box">
       <span class="menu"><v-icon size="35px">mdi-menu</v-icon></span>
       <span class="arrow-left-span">
@@ -16,6 +31,46 @@ import { reactive, onMounted } from "vue";
 import gsap from "gsap";
 
 onMounted(() => {});
+
+interface obj {
+  name: string;
+  isClick: boolean;
+}
+
+const menuList: Array<obj> = reactive([
+  {
+    name: "개요",
+    isClick: false,
+  },
+  {
+    name: "핵심 강점",
+    isClick: false,
+  },
+  {
+    name: "사용 기술",
+    isClick: false,
+  },
+  {
+    name: "경력",
+    isClick: false,
+  },
+  {
+    name: "연락처",
+    isClick: true,
+  },
+]);
+
+function menuClick(number: number) {
+  menuList
+    .filter((one) => {
+      if (one.isClick) return one;
+    })
+    .map((one) => {
+      one.isClick = false;
+    });
+
+  menuList[number].isClick = true;
+}
 </script>
 
 <style scoped>
@@ -24,7 +79,7 @@ onMounted(() => {});
 }
 .done {
   font-family: "Source Sans Pro";
-  padding: 58px 0 0 108px;
+  padding: 0 0 0 108px;
 }
 
 .menu-box {
@@ -59,5 +114,42 @@ onMounted(() => {});
   font-size: 17px;
   align-self: center;
   margin-bottom: 8px;
+}
+
+.menu-contents {
+  background-color: #000;
+  width: 460px;
+  height: 100vh;
+  position: absolute;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.name-div {
+  width: 360px;
+}
+
+.menu-contents .name {
+  color: #fff;
+  margin-bottom: 80px;
+  font-size: 28px;
+  cursor: pointer;
+}
+
+.done .line {
+  width: 350px;
+  height: 3px;
+  background-color: #fff;
+}
+
+.opacity-0 {
+  opacity: 0;
+}
+
+.opacity-1 {
+  opacity: 1;
 }
 </style>
